b30f5d4de353baf5129dac096c150d4c443bb27b
Gijs <gijs@studiogijs.nl>
2021-02-18 21:56:09 +0100

    Update projectedView.py

--- layout-tools/projectedView.py
+++ layout-tools/projectedView.py
@@ -2,6 +2,7 @@ import Rhino
 import scriptcontext as sc
 import rhinoscriptsyntax as rs
 from System.Drawing import Color
+from Rhino.Geometry import *
 
 
 def createProjectedDetail():
@@ -11,15 +12,15 @@ def createProjectedDetail():
     left/right from front view
     back from right or left view
     
-    known bugs: creating left or right views from top won't work correctly
-    
-    version 0.3
+    version 0.4
     new in version 0.2:
         - locked details should now work better
     new in 0.3:
         - projected views now have their cplane set to the view, which should result in better behavior when editing the detail
         - projected views from top or bottom views to right or left are ignored with user message.
         - message when trying to add projected view from perspective
+    new in 0.4:
+        - new details now have their title correctly set to the actual view projection
     www.studiogijs.nl
     """
 
@@ -27,11 +28,11 @@ def createProjectedDetail():
 
         vec = e.CurrentPoint-basept
         translate = vec
-        xf = Rhino.Geometry.Transform.Translation(translate)
-        plane = Rhino.Geometry.Plane(ptLL, Rhino.Geometry.Plane.WorldXY.ZAxis)
+        xf = Transform.Translation(translate)
+        plane = Plane(ptLL, Plane.WorldXY.ZAxis)
         sizeX = ptLR.X-ptLL.X #width of selected detail
         sizeY = ptUR.Y-ptLR.Y #height of selected detail
-        newobj = Rhino.Geometry.Rectangle3d(plane,sizeX,sizeY)
+        newobj = Rectangle3d(plane,sizeX,sizeY)
         crv  = newobj.ToNurbsCurve()
         
         preview=crv.Duplicate()
@@ -52,30 +53,32 @@ def createProjectedDetail():
     #set focus page and select detail
     pageview = sc.doc.Views.ActiveView
     pageview.SetPageAsActive()
-    detailview = rs.GetObject("select detail for projected view",32768, preselect=True)
-    if not detailview:
+    filter = Rhino.DocObjects.ObjectType.Detail
+    rc, objref = Rhino.Input.RhinoGet.GetOneObject("Select detail to rename", False, filter)
+    if not objref or rc != Rhino.Commands.Result.Success: 
         return
     
+    detail_obj = objref.Object()
+    if not isinstance(detail_obj, Rhino.DocObjects.DetailViewObject):
+        return
+    viewport = detail_obj.Viewport
     
-    detail = rs.coercerhinoobject(detailview)
-    
-    
-    if not detail.DetailGeometry.IsParallelProjection:
+    if not viewport.IsParallelProjection:
         print "Can't make projected views from perspective views"
         return
     
     
     #get lower left and lower right and upper right points of selected detail view
-    ptLL = Rhino.Geometry.Point3d(0,0,0)
-    ptLR = Rhino.Geometry.Point3d(0,0,0)
-    ptUR = Rhino.Geometry.Point3d(0,0,0)
+    ptLL = Point3d(0,0,0)
+    ptLR = Point3d(0,0,0)
+    ptUR = Point3d(0,0,0)
     
-    ptLL = detail.Geometry.GetBoundingBox(Rhino.Geometry.Plane.WorldXY).Min
+    ptLL = detail_obj.Geometry.GetBoundingBox(Plane.WorldXY).Min
     
-    ptLR.X = detail.Geometry.GetBoundingBox(Rhino.Geometry.Plane.WorldXY).Max.X
-    ptLR.Y = detail.Geometry.GetBoundingBox(Rhino.Geometry.Plane.WorldXY).Min.Y
+    ptLR.X = detail_obj.Geometry.GetBoundingBox(Plane.WorldXY).Max.X
+    ptLR.Y = detail_obj.Geometry.GetBoundingBox(Plane.WorldXY).Min.Y
     
-    ptUR = detail.Geometry.GetBoundingBox(Rhino.Geometry.Plane.WorldXY).Max
+    ptUR = detail_obj.Geometry.GetBoundingBox(Plane.WorldXY).Max
     
     basept = (ptLL + ptUR)/2 #center of Detail view
     
@@ -100,9 +103,8 @@ def createProjectedDetail():
         else:
             vectY = rs.VectorSubtract(ptUR, ptLR) # up
 
-    newdetail = rs.CopyObject(detailview, vectY)
+    newdetail = rs.CopyObject(detail_obj, vectY)
     d = rs.coercerhinoobject(newdetail)
-    
     
     view = sc.doc.Views.ActiveView
     view.SetActiveDetail(newdetail)
@@ -110,39 +112,72 @@ def createProjectedDetail():
     d.DetailGeometry.IsProjectionLocked = False
     
     VP = 0
+    newViewportName = ""
     
     if d.Viewport.CameraZ==Rhino.Geometry.Vector3d(0,0,1):
         #top view
+        viewportname = "top"
         VP=1
     elif d.Viewport.CameraZ==Rhino.Geometry.Vector3d(0,-1,0):
         #front view
+        viewportname = "front"
         VP=2
     elif d.Viewport.CameraZ==Rhino.Geometry.Vector3d(1,0,0):
         #right view
+        viewportname = "right"
         VP=3
     elif d.Viewport.CameraZ==Rhino.Geometry.Vector3d(0,0,-1):
         #bottom view
+        viewportname = "bottom"
         VP=4
     elif d.Viewport.CameraZ==Rhino.Geometry.Vector3d(-1,0,0):
         #left view
+        viewportname = "left"
         VP=5
     elif d.Viewport.CameraZ==Rhino.Geometry.Vector3d(0,1,0):
         #back view
+        viewportname = "back"
         VP=6
     else:
         return
-    d.CommitChanges()
+    
     if abs(vect.X)>abs(vect.Y): #make horizontal view
         if VP==1 or VP==4:
-            print"Can't make a right view of this view, try to make them from front, right, left or back views"
+            print"Can't make a correct projection of this view, try to make them from front, right, left or back views"
             rs.DeleteObject(newdetail)
             return
         if VP==2 or VP==6:
             newplane = Rhino.Geometry.Plane(Rhino.Geometry.Point3d.Origin, Rhino.Geometry.Plane.WorldXY.XAxis)
             rs.ViewCPlane(plane = newplane)
+            if viewportname == "front":
+                if vect.X<0:
+                    #left view from front
+                    newViewportName = "Left"
+                else:
+                    newViewportName = "Right"
+            
+            if viewportname == "back":
+                if vect.X<0:
+                    #left view from back
+                    newViewportName = "Right"
+                else:
+                    newViewportName = "Left"
+            
         elif VP==3 or VP==5:
             newplane = Rhino.Geometry.Plane(Rhino.Geometry.Point3d.Origin, Rhino.Geometry.Plane.WorldXY.YAxis)
             rs.ViewCPlane(plane = newplane)
+            if viewportname == "left":
+                if vect.X<0:
+                    #left view from left
+                    newViewportName = "Back"
+                else:
+                    newViewportName = "Front"
+            if viewportname == "right":
+                if vect.X<0:
+                    #left view from right
+                    newViewportName = "Front"
+                else:
+                    newViewportName = "Back"
     
         if vect.X<0:
             
@@ -155,15 +190,35 @@ def createProjectedDetail():
         if VP==1 or VP==4:
             newplane = Rhino.Geometry.Plane(Rhino.Geometry.Point3d.Origin, Rhino.Geometry.Plane.WorldXY.YAxis)
             rs.ViewCPlane(plane = newplane)
+            if viewportname == "top":
+                if vect.Y<0:
+                    #front view from top
+                    newViewportName = "Front"
+                else:
+                    newViewportName = "Back"
+            if viewportname == "bottom":
+                if vect.Y<0:
+                    #back view from bottom
+                    newViewportName = "Back"
+                else:
+                    newViewportName = "Front"
         elif VP==2 or VP==3 or VP==5 or VP==6:
             newplane = Rhino.Geometry.Plane(Rhino.Geometry.Point3d.Origin, Rhino.Geometry.Plane.WorldXY.ZAxis)
             rs.ViewCPlane(plane = newplane)
+            if vect.Y<0:
+                #bottom view from front, right, left or back
+                newViewportName = "Bottom"
+            else:
+                newViewportName = "Top"
         if vect.Y<0:
             rs.RotateView(direction = 3,angle = 90)#rotate down
         else:#Y>0
             rs.RotateView(direction = 2,angle = 90)#rotate up
     #d.DetailGeometry.IsProjectionLocked = True
-    d.CommitChanges()
+    viewport = d.Viewport
+    title = viewport.Name
+    viewport.Name = newViewportName
+    d.CommitViewportChanges()
     sc.doc.Views.ActiveView.SetPageAsActive()
     sc.doc.Views.Redraw()
 
